521ed985bb20743e1e3b634f55ac78b9f3a59b5c
NgNhatThanh <thanhnn.b22cn795@stu.ptit.edu.vn>
2026-03-27 16:31:44 +0700

    feat(ThanhNN): xem danh sách phim

---
+++ app/src/main/java/com/nhom2/mini_project_2/model/dao/MovieDao.java
@@ -0,0 +1,14 @@
+package com.nhom2.mini_project_2.model.dao;
+
+import androidx.room.Dao;
+import androidx.room.Query;
+
+import com.nhom2.mini_project_2.model.entity.MovieEntity;
+
+import java.util.List;
+
+@Dao
+public interface MovieDao {
+    @Query("SELECT * FROM movies ORDER BY id ASC")
+    List<MovieEntity> getAllMovies();
+}
